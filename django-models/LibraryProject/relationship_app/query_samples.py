de3c36dc90e4798ff42478c205f466742f54a552
bereketa <beckyamare1@gmail.com>
2024-11-10 01:21:05 +0300

    Add content of django-models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,21 +1,37 @@
+import os
+import django
+
+# Set up Django environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+django.setup()
+
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
+# Query all books by a specific author using filter
 def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    books = author.books.all()
-    for book in books:
-        print(book.title)
+    try:
+        author = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)  # Using filter to query by author
+        for book in books:
+            print(book.title)
+    except Author.DoesNotExist:
+        print(f"No author found with the name: {author_name}")
 
 # List all books in a library
 def get_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    books = library.books.all()
-    for book in books:
-        print(book.title)
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        for book in books:
+            print(book.title)
+    except Library.DoesNotExist:
+        print(f"No library found with the name: {library_name}")
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    librarian = library.librarian
-    print(f"Librarian for {library_name}: {librarian.name}")
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = library.librarian
+        print(f"Librarian for {library_name}: {librarian.name}")
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        print(f"No librarian found for the library: {library_name}")
