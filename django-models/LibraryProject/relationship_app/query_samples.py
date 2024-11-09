9a1dfd04bd5f266bd6c60a6a07e44b0b783889dd
bereketa <beckyamare1@gmail.com>
2024-11-10 01:23:44 +0300

    Add content of django-models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,3 +1,7 @@
+ersion of your query_samples.py script with the missing filter query:
+
+python
+Copy code
 import os
 import django
 
@@ -5,33 +9,20 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
+# Import models
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author using filter
+# Query all books by a specific author
 def get_books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        books = Book.objects.filter(author=author)  # Using filter to query by author
-        for book in books:
-            print(book.title)
-    except Author.DoesNotExist:
-        print(f"No author found with the name: {author_name}")
+    # Get the author object based on the author's name
+    author = Author.objects.get(name=author_name)
+    
+    # Use filter to get all books written by the author
+    books = Book.objects.filter(author=author)
+    
+    # Print out the books
+    for book in books:
+        print(book.title)
 
-# List all books in a library
-def get_books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        books = library.books.all()
-        for book in books:
-            print(book.title)
-    except Library.DoesNotExist:
-        print(f"No library found with the name: {library_name}")
-
-# Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        librarian = library.librarian
-        print(f"Librarian for {library_name}: {librarian.name}")
-    except (Library.DoesNotExist, Librarian.DoesNotExist):
-        print(f"No librarian found for the library: {library_name}")
+# Example call to the function (replace 'Author Name' with an actual author's name)
+get_books_by_author('Author Name')
