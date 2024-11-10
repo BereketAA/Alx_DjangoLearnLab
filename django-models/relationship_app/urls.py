19bfb3ed6128de36dcd570f4afe2b4efa7bf6f5b
bereketa <beckyamare1@gmail.com>
2024-11-10 04:33:20 +0300

    Add content of django-models

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth import views as auth_views  # Import Django's built-in 
 urlpatterns = [
     path('register/', views.register, name='register'),  # Custom registration view
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),  # Built-in login view
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Built-in logout view
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),  # Built-in logout view
     path('logged_out/', views.logged_out, name='logged_out'),  # Optional logged-out page
     path('books/', views.book_list, name='book_list'),  # Other URL patterns...
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
