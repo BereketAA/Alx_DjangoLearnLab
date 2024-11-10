9255a1a0cf9f3202ba3b30f4fbe493d4fc770d17
bereketa <beckyamare1@gmail.com>
2024-11-10 03:02:23 +0300

    Add content of django-models

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,13 @@
-f# relationship_app/views.py
-
 from django.shortcuts import render
-from django.views.generic.detail import DetailView  # Add this import
-from .models import Book, Library
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Book
+
+from django.views.generic import DetailView
+from .models import Library
+
+# relationship_app/views.py
 
 # Function-based view to list all books and their authors
 def book_list(request):
@@ -10,10 +15,12 @@ def book_list(request):
     context = {
         'books': books,
     }
-    return render(request, 'relationship_app/list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)  # Use the correct template path
+
 
 # Class-based view to display details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
