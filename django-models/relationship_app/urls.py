8142ea6eab9cc5be2cccfa66c04e969d60d45483
bereketa <beckyamare1@gmail.com>
2024-11-10 03:58:49 +0300

    Add content of django-models

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,14 +1,15 @@
 # relationship_app/urls.py
 
 from django.urls import path
-from . import views
-from .views import book_list, LibraryDetailView 
+from . import views  # Import all views from views.py
 
 urlpatterns = [
     # URL for function-based view to list all books
-    path('books/', views.book_list, name='book_list'),
+    path('books/', views.book_list, name='book_list'),  # Function-based view for books list
 
     # URL for class-based view to display a library's details
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for library details
+
+    # Optional ListView for all libraries (if you want to list all libraries)
     path('libraries/', views.LibraryListView.as_view(), name='library_list'),  # Optional ListView for all libraries
 ]
