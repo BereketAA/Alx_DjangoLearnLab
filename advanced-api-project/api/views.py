3233ae5263d77beab84a22b4ae4935f888027c65
bereketa <beckyamare1@gmail.com>
2024-11-30 01:45:34 +0300

    Add content of advanced-api-project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,6 +6,7 @@ from django_filters import rest_framework as filters
 from rest_framework import generics
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 # Create your views here.
 
