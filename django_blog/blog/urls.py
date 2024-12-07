ec39ff0fcdf612d7b185181225046adeb15c7065
bereketa <beckyamare1@gmail.com>
2024-12-08 02:24:47 +0300

    Add content of django_blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -31,8 +31,8 @@ from django.urls import path
 from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
 
 urlpatterns += [
-    path('post/<int:pk>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('post/<int:post_id>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
