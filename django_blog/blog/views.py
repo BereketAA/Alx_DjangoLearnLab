b8ec20b65fa9e274b55e1fd972fb89a08e7fe80c
bereketa <beckyamare1@gmail.com>
2024-12-07 18:39:54 +0300

    Add content of django_blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.contrib.auth.views import LoginView, LogoutView
+
+path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
+path('logout/', LogoutView.as_view(), name='logout'),
+
+
+
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
