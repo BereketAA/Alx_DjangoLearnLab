fc2d58236d115b2ba8d3b2bbd4a58b13a7bf4d42
bereketa <beckyamare1@gmail.com>
2024-12-13 15:27:44 +0300

    Add content of social_media_api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,22 +1,40 @@
 from rest_framework import serializers
 from .models import CustomUser
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    # Explicitly define a password field with write-only restriction
+    password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'token']  # Add required fields
+        
+    def create(self, validated_data):
+        # Create the user instance
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        # Create a token for the user
+        token = Token.objects.create(user=user)
+        user.token = token.key  # Attach the token for serialization
+        return user
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
+    
+        
+        
