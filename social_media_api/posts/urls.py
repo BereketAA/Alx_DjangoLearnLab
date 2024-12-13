939eabe07a82166a1a69baff3990dc943e363887
bereketa <beckyamare1@gmail.com>
2024-12-14 01:57:34 +0300

    Add content of social_media_api

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('feed/', FeedView.as_view(), name='feed'),
     path('<int:pk>/like/', LikeView.as_view(), name='like-post'),
+    path('posts/<int:pk>/unlike/', LikeView.as_view(), name='unlike-post'),
 ]
