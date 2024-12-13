71f61b1adbfc0a1b75afed7eb5fc8a1f0815d47c
bereketa <beckyamare1@gmail.com>
2024-12-14 02:26:58 +0300

    Add content of social_media_api

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'admin'),
+        'USER': os.getenv('DB_USER', 'user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '123'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),  # Default PostgreSQL port
     }
 }
 
